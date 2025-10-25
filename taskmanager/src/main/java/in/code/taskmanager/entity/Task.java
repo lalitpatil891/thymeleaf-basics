5d4e9b0302505388b6ec2ff750ae2c071c3d7520
lalitpatil891 <lp613408@gmail.com>
2025-10-25 23:01:31 +0530

    add task Manager project structure

---
+++ taskmanager/src/main/java/in/code/taskmanager/entity/Task.java
@@ -0,0 +1,26 @@
+package in.code.taskmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private String priority;
+    private String status;
+
+}
