6a8484f7856fbccb4bb77ae2264ecd1ee2af592d
lalitpatil891 <lp613408@gmail.com>
2025-10-27 23:29:19 +0530

    add serach feature

--- bookstore/src/main/java/com/bookstore/controller/BookController.java
+++ bookstore/src/main/java/com/bookstore/controller/BookController.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bookstore.entity.Book;
@@ -93,5 +93,14 @@ public class BookController {
 		bookService.deleteBook(id);
 		return "redirect:/available_books";
 	}
+	
+	//Search feature
+    @GetMapping("/search")
+    public String searchBooks(@RequestParam("query") String query, Model model) {
+        List<Book> result = bookService.searchBooksByStartingLetter(query);
+        model.addAttribute("book", result);
+        return "bookList";
+    }
+
 	
 }
--- bookstore/src/main/java/com/bookstore/service/BookService.java
+++ bookstore/src/main/java/com/bookstore/service/BookService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bookstore.entity.Book;
 import com.bookstore.repository.BookRepository;
@@ -30,4 +32,9 @@ public class BookService {
 	public void deleteBook(int id) {
 		bookRepo.deleteById(id);
 	}
+
+	// New method for searching alphabet-wise
+    public List<Book> searchBooksByStartingLetter(String query) {
+        return bookRepo.findByNameStartingWithIgnoreCase(query);
+    }
 }
